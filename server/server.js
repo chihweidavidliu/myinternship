b226131bf1f532c1cbbabf3532d3e1b95fe22e2c
Chih-Wei <chihweiliu1993@gmail.com>
2018-11-13 16:57:18 +0100

    Refactored code to put loading companyChoices into separate middleware

--- server/server.js
+++ server/server.js
@@ -5,6 +5,7 @@ const {Admin} = require('./models/admin');
 
 const {authenticate} = require('./middleware/authenticate.js');
 const {authenticateAdmin} = require('./middleware/authenticateAdmin.js');
+const {loadCompanyChoices} = require('./middleware/loadCompanyChoices.js');
 
 const hbs = require('hbs');
 const {ObjectID} = require('mongodb'); // import ObjectID from mongodb for id validation methods
@@ -135,72 +136,15 @@ app.post("/admin", urlencodedParser, (req, res) => {
 
 
 // show logged in page for admin
-app.get("/admin/:token", authenticateAdmin, (req, res, next) => {
+app.get("/admin/:token", authenticateAdmin, loadCompanyChoices, (req, res, next) => {
 
-    if(req.admin.companyChoices && req.admin.companyChoices.length > 0) {
-      let companyChoices = JSON.parse(req.admin.companyChoices); // don't need to parse JSOn because before saving to database was converted back to array
-      let headers = '<th scope="col">Company</th><th scope="col">Number Accepted</th>';
-      let rows = "";
-
-
-
-      companyChoices.forEach(company => {
-        rows += '<tr>';
-
-
-          company.forEach(entry => {
-            if(company.indexOf(entry) == 0) { // identify the company name (position 0 in index)
-              rows += `<th scope="row" contenteditable="true">${entry}</th>`; // give company names head value (bolded)
-            } else {
-              rows += `<td scope="col" contenteditable="true">${entry}</td>`; // give the other column entries a normal table data value (not bolded)
-            }
-          })
-
-        rows += '</tr>';
-      })
-
-      console.log(rows);
-
-      function columnCount(array) {
-               let max = 0;
-               companyChoices.forEach(company => {
-                 if(company.length > max) { max = company.length;}
-               })
-               return max;
-           }
-
-      let numberOfColumns = columnCount(companyChoices);
-
-      // add the requisite number of 'choices' column headers
-      for(let i = 1; i < (numberOfColumns -1); i++) {
-        headers += `<th scope="col" style="text-align: center">Choice ${i}</th>`;
-      }
-
-
-      let table = `<h4>Company Choices</h4>
-                  <div id="tableFlexBox">
-                      <div id="buttons">
-                          <button id="addRow">Add row</button>
-                          <button id="removeRow">Remove row</button>
-                          <button id="addColumn">Add column</button>
-                          <button id="removeColumn">Remove column</button>
-                          <button id="submitChoices">Save</button>
-                      </div>
-                      <div id = "tableDiv" style="overflow-x:scroll; overflow-y: scroll">
-                          <table class="table table-hover table-bordered table-sm" id="companyChoices">
-                            <thead>
-                              <tr id="headers">
-                                ${headers}
-                              </tr>
-                            </thead>
-                            <tbody id="tableBody2">${rows}</tbody>
-                          </table>
-                      </div>
-                  </div>`;
+    if(req.table) {
+      let table = req.table;
 
       return res.render('loggedInAdmin.hbs', {
         table: table,
       })
+
     }
 
     res.render('loggedInAdmin.hbs')
